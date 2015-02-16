717af2e189d67761f01f7eda6d1367c3d9cda8ff
Wiliam Kudaka <wiliamkudaka@gmail.com>
2015-02-16 16:40:34 -0200

    removing some tests

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -19,38 +19,5 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe PhotosController, type: :controller do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Photo. As you add validations to Photo, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PhotosController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET #index" do
-    it "assigns all photos as @photos" do
-      photo = Photo.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:photos)).to eq([photo])
-    end
-  end
-
-  describe "GET #show" do
-    it "assigns the requested photo as @photo" do
-      photo = Photo.create! valid_attributes
-      get :show, {:id => photo.to_param}, valid_session
-      expect(assigns(:photo)).to eq(photo)
-    end
-  end
-
-  
+  pending "add some examples to (or delete) #{__FILE__}"
 end
